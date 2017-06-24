7d66921befec41a26f97bb829c65885921804bda
Matthew Myers <matt2east@gmail.com>
2017-06-24 15:01:11 -0500

    hackathon initial commit, landing page finished

---
+++ hackathon/script.js
@@ -0,0 +1,14 @@
+function signupScroll() {
+    var elmnt = document.getElementById("signupiv");
+    elmnt.scrollIntoView();
+}
+
+function overviewScroll() {
+    var elmnt = document.getElementById("overviewdiv");
+    elmnt.scrollIntoView();
+}
+
+function howitworksScroll() {
+    var elmnt = document.getElementById("howitworksdiv");
+    elmnt.scrollIntoView();
+}
