f09cf021907ad89338b6d0d57c02868ed458ec11
Matthew Myers <matt2east@gmail.com>
2017-09-13 21:02:03 -0500

    got image and hide to work

--- reactproject/App.jsx
+++ reactproject/App.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 
+class Child extends React.Component {
+  render() {
+    return (<div>Enter a URL to get a screenshot.</div>);
+  }
+}
+
 class ChildProps extends React.Component {
   render() {
     return <div><h1>{this.props.headerProp}</h1>
@@ -16,7 +22,7 @@ ChildProps.defaultProps = {
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: '', data: ''};
+    this.state = {value: '', data: '', childVisible: true};
      this.updateState = this.updateState.bind(this);  
 
     this.handleChange = this.handleChange.bind(this);
@@ -37,7 +43,7 @@ class App extends React.Component {
       let api = "http://api.screenshotmachine.com/?key=2df3a8&dimension=1024xfull&format=png&url=";  
     let input = this.state.value;
     let newVal = api + input;    
-    this.setState({value: '', data: newVal})
+    this.setState({value: '', data: newVal, childVisible: !this.state.childVisible})
    }
     
      
@@ -48,11 +54,18 @@ class App extends React.Component {
           onSubmit={this.handleSubmit}
           >
         <label>
-          Enter a URL to get a screenshot.<br></br>
+            <div>
+        {
+          this.state.childVisible
+            ? <Child />
+            : null
+        }
+            </div>
+            <br></br>
           <input type="text" value={this.state.value} placeholder="text" onChange={this.handleChange} />
         </label><br></br>
         <input type="submit" value="Submit" onClick = {this.updateState}/>
-        <h2>{this.state.data}</h2>
+        <img src={this.state.data}></img>
             <ChildProps></ChildProps>
       </form>
     );
