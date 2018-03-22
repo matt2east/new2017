e948d55416401c0f6c32858fc491e17ae238776c
Matthew Myers <matt2east@gmail.com>
2018-03-22 15:55:14 -0500

    issue with axios

---
+++ merntut/react-frontend/src/index.js
@@ -0,0 +1,20 @@
+// index.js
+
+
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import App from './App';
+import AddItem from './components/AddItem';
+
+ReactDOM.render(
+  <Router>
+      <div>
+        <Route exact path='/' component={App} />
+        <Route path='/add-item' component={AddItem} />
+      </div>
+  </Router>,
+  document.getElementById('root')
+);
