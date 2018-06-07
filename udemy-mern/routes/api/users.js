b0d00bd964b5351ff4095e3885ae9196be47039c
Matthew Myers <matt2east@gmail.com>
2018-06-06 21:54:10 -0500

    postman works on server

--- udemy-mern/routes/api/users.js
+++ udemy-mern/routes/api/users.js
@@ -1,8 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+ 
+// Load User model
+const User = require('../../models/User');
 
 router.get('/test', (req,res)=> res.json({
   msg: 'users works'
 }));
+
+router.post('/register', (req, res)=>{
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      errors.email = 'Email already exists';
+      return res.status(400).json(errors);
+    } else {
+  
+
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        // avatar,
+        password: req.body.password
+      });
+
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
+  });
+}) 
+
 
 module.exports = router;
--- udemy-mern/server.js
+++ udemy-mern/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
 const posts = require('./routes/api/posts');
 
 const app = express();
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json());
 
 //DB config
 const db = require('./config/keys').mongoURI;
