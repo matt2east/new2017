bb2445d38a598b9f6eb1e44506730aa5b609fe2b
Matthew Myers <matt2east@gmail.com>
2018-06-20 22:41:28 -0500

    finished lesson 55

---
+++ devconnector_S9/client/src/components/add-credentials/AddEducation.js
@@ -0,0 +1,55 @@
+// import React, { Component } from "react";
+// import { Link, withRouter } from "react-router-dom";
+// import { TextFieldGroup } from "../common/TextFieldGroup";
+// import { TextAreaFieldGroup } from "../common/TextAreaFieldGroup";
+// import { connect } from "react-redux";
+// import PropTypes from "prop-types";
+
+// class AddEducation extends Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = {
+//       company: "",
+//       title: "",
+//       location: "",
+//       from: "",
+//       to: "",
+//       current: false,
+//       description: "",
+//       error: {},
+//       disabled: false
+//     };
+//   }
+//   render() {
+//     const { errors } = this.state;
+//     // same as const errors = this.state.errors
+//     return (
+//       <div className="add-experience">
+//         <div className="container">
+//           <div className="row">
+//             <div className="col-md-8 m-auto">
+//               <Link to="/dashboard" className="btn btn-light">
+//                 Go Back
+//               </Link>
+//               <h1 className="display-4 text-center" />
+//               <p className="lead text-center">Add any job or position that you have had in the past or current.</p>
+//               <small className="d-block pb-3">* = required fields</small>
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// AddExperience.PropTypes = {
+//   profile: PropTypes.object.isRequired,
+//   errors: PropTypes.object
+// };
+
+// const mapStateToProps = state => ({
+//   profile: state.profile,
+//   errors: state.errors
+// });
+
+// export default connect(mapStateToProps)(withRouter(AddEducation));
