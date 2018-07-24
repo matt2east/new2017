201a8e9772ad1d582d72f9b7e1987f83961d9214
Matthew Myers <matt2east@gmail.com>
2018-07-24 16:16:43 -0500

    react router set up

---
+++ goalcoach/goalcoach/src/components/SignUp.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class SignUp extends Component {
+render (){
+  return (
+    <div>SignUp Component</div>
+  )
+}
+}
+
+export default SignUp;
--- goalcoach/goalcoach/src/index.js
+++ goalcoach/goalcoach/src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import App from './components/App';
+import SignUp from './components/SignUp';
+import SignIn from './components/SignIn';
+import {Router, Route, browserHistory} from 'react-router'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
+ReactDOM.render(
+  <Router path="/"
+  history={browserHistory}>
+  <Route path="/app" 
+ component={App} />
+   <Route path="/signin" 
+ component={SignIn} />
+   <Route path="/signup" 
+ component={SignUp} />
+
+
+  </Router>, document.getElementById('root')
+)
