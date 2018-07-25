bd12c4fddb9bc25d3ad8caa4e8c8b498844e6b06
Matthew Myers <matt2east@gmail.com>
2018-07-25 17:09:07 -0500

    not finding user object on addGoal

--- goalcoach/goalcoach/src/firebase.js
+++ goalcoach/goalcoach/src/firebase.js
@@ -6,8 +6,12 @@ const config = {
     authDomain: "goalcoach-2f3da.firebaseapp.com",
     databaseURL: "https://goalcoach-2f3da.firebaseio.com",
     projectId: "goalcoach-2f3da",
-    storageBucket: "",
+    storageBucket: "gs://goalcoach-2f3da.appspot.com",
     messagingSenderId: "818274323735"
   };
 
   export const firebaseApp = firebase.initializeApp(config);
+
+  export const goalRef = firebase.database().ref('goals');
+  
+ 
