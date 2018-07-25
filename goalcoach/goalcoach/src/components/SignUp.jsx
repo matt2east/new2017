9eb63f2128b24c298f7eef36b047fd2a5de55b79
Matthew Myers <matt2east@gmail.com>
2018-07-24 22:35:20 -0500

    finished signup component with firebase authentication

--- goalcoach/goalcoach/src/components/SignUp.jsx
+++ goalcoach/goalcoach/src/components/SignUp.jsx
@@ -1,29 +1,62 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
+import { firebaseApp } from "../firebase";
 
 class SignUp extends Component {
-render (){
-  return (
-    <div className="form-inline">
-    <h2>Sign up</h2>
-    <div className="form-group">
-    <input 
-    className="form-control"
-    placholder="password"
-    type="password"
-    />
-        <input 
-    className="form-control"
-    placholder="email"
-    type="text"
-    />
-    <button className="btn btn-primary"
-    type="button">
-    Sign Up
-    </button>
-    </div>
-    </div>
-  )
-}
+  constructor(props) {
+    super(props);
+    this.state = {
+      email: "",
+      password: "",
+      error: {
+        message: ''
+      }
+    };
+  }
+
+  signUp() {
+    console.log("this.state", this.state);
+    const { email, password } = this.state;
+    firebaseApp
+      .auth()
+      .createUserWithEmailAndPassword(email, password)
+      .catch(error => {
+        console.log(error);
+        this.setState({error})
+      });
+  }
+
+  render() {
+    return (
+      <div className="form-inline"
+      style={{margin: '5%'}}>
+        <h2>Sign up</h2>
+        <div className="form-group">
+          <input
+            className="form-control"
+            type="text"
+            placeholder="email"
+            style={{marginRight: '5px'}}
+            onChange={event => this.setState({ email: event.target.value })}
+          />
+          <input
+            className="form-control"
+            type="password"
+            placeholder="password"
+            style={{marginRight: '5px'}}
+            onChange={event => this.setState({ password: event.target.value })}
+          />
+          <button
+            className="btn btn-primary"
+            type="button"
+            onClick={() => this.signUp()}
+          >
+            Sign Up
+          </button>
+        </div>
+        <div>{this.state.error.message}</div>
+      </div>
+    );
+  }
 }
 
 export default SignUp;
