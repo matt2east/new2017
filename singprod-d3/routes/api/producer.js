1d22a02c71fb7fde8992845e20f01d6da4a98234
Matthew Myers <matt2east@gmail.com>
2018-08-30 21:21:46 -0500

    producer profile validation added

--- singprod-d3/routes/api/producer.js
+++ singprod-d3/routes/api/producer.js
@@ -6,16 +6,19 @@ const passport = require('passport');
 //load User Model
 const User = require('../../models/User');
 
-//load Singer Model
+//load Producer Model
 const Producer = require('../../models/Producer');
 
-// @route   GET api/singer/test
+// Load Validation
+const validateProducerInput = require('../../validation/producer');
+
+// @route   GET api/producer/test
 // @desc    Tests profile route
 // @access  Public
-// router.get('/test', (req, res) => res.json({ msg: 'Singer test Works' }));
+// router.get('/test', (req, res) => res.json({ msg: 'Producer test Works' }));
 
-//@route GET /api/singer/
-//@desc get current singer profile
+//@route GET /api/producer/
+//@desc get current producer profile
 // @access  Private
 // @route   GET api/profile
 // @desc    Get current users profile
@@ -39,6 +42,76 @@ router.get(
   }
 );
 
+//@route POST /api/producer/
+//@desc create current producer profile
+// @access  Private
+// @route   POST api/profile
+// @desc    Get current producer profile
+// @access  Private
+router.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validateProducerInput(req.body);
 
+    // Check Validation
+    if (!isValid) {
+      // Return any errors with 400 status
+      return res.status(400).json(errors);
+    }
+
+    // Get fields
+    const producerFields = {};
+   producerFields.user = req.user.id;
+    if (req.body.moniker) producerFields.moniker = req.body.moniker;
+    if (req.body.bio) producerFields.bio = req.body.bio;
+    if (req.body.email) producerFields.email = req.body.email;
+    if (req.body.website) producerFields.website = req.body.website;
+    if (req.body.pic) producerFields.pic = req.body.pic;
+    if (req.body.demo1) producerFields.demo1 = req.body.demo1;
+    if (req.body.demo2) producerFields.demo2 = req.body.demo2;
+    if (req.body.demo3) producerFields.demo3 = req.body.demo3;
+    if (req.body.collab) producerFields.collab = req.body.collab;
+    if (req.body.paid) producerFields.paid = req.body.paid;
+    if (req.body.songwriter)
+      producerFields.songwriter = req.body.songwriter;
+    // Skills - Spilt into array
+    // if (typeof req.body.skills !== 'undefined') {
+    //   profileFields.skills = req.body.skills.split(',');
+    // }
+
+    // Social
+    // profileFields.social = {};
+    // if (req.body.youtube) profileFields.social.youtube = req.body.youtube;
+    // if (req.body.twitter) profileFields.social.twitter = req.body.twitter;
+    // if (req.body.facebook) profileFields.social.facebook = req.body.facebook;
+    // if (req.body.linkedin) profileFields.social.linkedin = req.body.linkedin;
+    // if (req.body.instagram) profileFields.social.instagram = req.body.instagram;
+
+    Producer.findOne({ user: req.user.id }).then(producer => {
+      if (producer) {
+        // Update
+        Producer.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: producerFields },
+          { new: true }
+        ).then(producer => res.json(producer));
+      } else {
+        // Create
+
+        // Check if handle exists
+        Producer.findOne({ moniker: producerFields.moniker }).then(producer => {
+          if (producer) {
+            errors.moniker = 'That moniker already exists.';
+            res.status(400).json(errors);
+          }
+
+          // Save Profile
+          new Producer(producerFields).save().then(producer => res.json(producer));
+        });
+      }
+    });
+  }
+);
 
 module.exports = router;
--- singprod-d3/validation/producer.js
+++ singprod-d3/validation/producer.js
@@ -1,7 +1,7 @@
 const Validator = require('validator');
 const isEmpty = require('./is-empty');
 
-module.exports = function validateSingerInput(data) {
+module.exports = function validateProducerInput(data) {
   let errors = {};
 
   data.moniker = !isEmpty(data.moniker) ? data.moniker : '';
