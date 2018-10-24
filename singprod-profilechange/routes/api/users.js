f046da2c0f3a77cb5b3a43427d4a8a882fb83c82
Matthew Myers <matt2east@gmail.com>
2018-10-24 18:16:22 -0500

    prepared for deploy

--- singprod-profilechange/routes/api/users.js
+++ singprod-profilechange/routes/api/users.js
@@ -52,7 +52,7 @@ let mailOptions = {
 //   from: '"Matthew Myers"', // sender address
   to: req.body.email, // list of receivers
   subject: "You signed up for Vox:Mix", // Subject line
-  text: "Your email is: " + req.body.email + " and your password is: " + req.body.password + ". Thank you!", // plain text body
+  text: "Your email is: " + req.body.email + " and your password is: " + req.body.password + ". Thank you for joining!", // plain text body
 //   html: '<b>NodeJS Email Tutorial</b>' // html body
 };
 //send nodemailer upon signup
